d7f03401608428442e9b5f8fc14880ab8d60ff12
十七 <80404188+1xingao@users.noreply.github.com>
2021-10-04 18:01:07 +0800

    Update LAJI.py

--- python/main/LAJI.py
+++ python/main/LAJI.py
@@ -1,12 +1,15 @@
 #写各种脚本的文件
 #作者：xinao-seven
-#用处：数字图像处理结果计算
+#用处：狗子的机组作业
 
 import xlwt
+
 tage_list = ["型号","CPU","显卡","硬盘","内存","屏幕"]
 data_list_1 = ["暗影精灵7plus","i7-11800H","RTX-3080","三星PM9A1 1TB","16G DDR4-3200MHz","17.3寸2kIPS"]
-date_list_2 = ["华为matebook13s","i7-11370H","Intel Iris Xe Graphics核显","三星PM981a 512GB","LPDDR4X-3733MHz","13.4寸2kIPS"]
-data_list_3 = ["联想拯救者Y7000P","i7-11800H","RTX-3050Ti","三星PM9A1 512GB"," 16G DDR4-3200MHz","15.6寸1080pIPS"]
+date_list_2 = ["华为matebook13s","i7-11370H","Intel Iris Xe Graphics核显","三星PM981a 512GB","LPDDR4X-3733MHz",
+"13.4寸2kIPS"]
+data_list_3 = ["联想拯救者Y7000P","i7-11800H","RTX-3050Ti","三星PM9A1 512GB"," 16G DDR4-3200MHz",
+"15.6寸1080pIPS"]
 data_list_4 = ["华硕飞行堡垒9","i7-11400H","RTX-3050","三星PM991a 512GB"," 16G DDR4-3200MHz","15.6寸1080pIPS"]
 data_list_5 = ["ThinkBook14p","R7-5800H","vega8核显","海力士PC711 512GB"," 16G DDR4-3200MHz","14寸2kIPS"]
 data_list_6 = ["ThinkPad X1 Carbon 2021","i7-1165G7","英特尔锐炬® Xe 显卡","三星PM9A1 1TB",
