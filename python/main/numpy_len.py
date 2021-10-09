1d291fe90526da6be46da02389739979d587fd15
十七 <80404188+1xingao@users.noreply.github.com>
2021-10-09 21:12:38 +0800

    7

--- python/main/numpy_len.py
+++ python/main/numpy_len.py
@@ -13,7 +13,8 @@ print(mp.shape)
 mp_1 = np.arange(0, 10, 2, dtype=np.int8)
 
 #arange函数可指定起始，结束，步长，类型
-#同类型的还有linspace函数它将步长参数换位所指定的数组元素个数
+
+#同类型的还有linspace函数它将步长参数换位所指定的数组元素个数1
 print("mp_1",mp_1)
 
 mp_3 = np.eye(2, 2)
