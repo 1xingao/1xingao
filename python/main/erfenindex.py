2fcd85901f6420107a8376fbf28e1076b9c23dc0
十七 <80404188+1xingao@users.noreply.github.com>
2021-10-14 17:34:43 +0800

    添加注释

--- python/main/erfenindex.py
+++ python/main/erfenindex.py
@@ -1,6 +1,6 @@
 from typing import List
 
-
+#寻找山峰数组的山峰
 #二分查找
 def peakIndexInMountainArray( arr: List[int]) -> int:
     left,right = -1,len(arr)
@@ -15,3 +15,5 @@ def peakIndexInMountainArray( arr: List[int]) -> int:
 data_list = [1,3,5,4,2,1]
 c=peakIndexInMountainArray(data_list)
 print(c)
+#二分查找算法的边界left和right一一对应
+#
