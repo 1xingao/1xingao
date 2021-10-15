80cfc8a7ca7afa6ee2771c74c6ee045b6bbda619
十七 <80404188+1xingao@users.noreply.github.com>
2021-10-15 22:21:39 +0800

    优化

--- python/main/leet_20.py
+++ python/main/leet_20.py
@@ -2,16 +2,15 @@
 有效的括号
 
 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
-
 有效字符串需满足：
-
 左括号必须用相同类型的右括号闭合。
 左括号必须以正确的顺序闭合。
- 
 
 """
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s)%2 != 0:
+            return False
         top = -1     #栈顶指针
         data1 = ["(","{","["]
         data2 = [")","}","]"]
@@ -31,6 +30,6 @@ class Solution:
                 return False
         return True if  not zhan else False
 c = Solution()
-str_te = "]"
+str_te = "(((()))))"
 ret = c.isValid(str_te)
 print(ret)
