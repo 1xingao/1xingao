bb5d1ce6b17d4bba706c37ccb856aba97e673e61
十七 <80404188+1xingao@users.noreply.github.com>
2022-01-21 16:02:08 +0800

    Create 145.二叉树的后序遍历.cpp

---
+++ leetcode/145.二叉树的后序遍历.cpp
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=cpp
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> ans;
+    vector<int> postorderTraversal(TreeNode* root) {
+        calc(root);
+
+        return ans;
+    }
+    void calc(TreeNode* root)
+    {
+        if (root){
+            calc(root->left);
+            calc(root->right);
+            ans.push_back(root->val);
+        }
+    }
+};
+// @lc code=end
+
