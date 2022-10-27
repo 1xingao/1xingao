e09c71964603743cffd0d549ef2fc6d97766b96c
十七 <80404188+1xingao@users.noreply.github.com>
2022-10-27 19:22:20 +0800

    新的前缀和类

--- cpp/new_soluation.cpp
+++ cpp/new_soluation.cpp
@@ -1,7 +1,7 @@
 /*
  * @Author: xinao_seven_
  * @Date: 2022-07-14 11:26:46
- * @LastEditTime: 2022-10-27 19:08:36
+ * @LastEditTime: 2022-10-27 19:21:17
  * @LastEditors: xinao_seven_
  * @Description:
  * @FilePath: \\1xingao\\cpp\\new_soluation.cpp
@@ -415,24 +415,20 @@ public:
 };
 
 //二维矩阵前缀和
-vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        vector<vector<int>> pre(mat.size()+1,vector<int>(mat[0].size()+1,0));
-        for ( int i=1;i<=mat.size();i++){
-            for(int j=1;j<=mat[0].size();j++){
-                pre[i][j] = pre[i-1][j] + mat[i-1][j-1] + pre[i][j-1]-pre[i-1][j-1];
+class NumMatrix {
+public:
+    vector<vector<int>> pre;
+    NumMatrix(vector<vector<int>>& matrix) {
+        pre = vector<vector<int>>(matrix.size()+1,vector<int>(matrix[0].size()+1));
+        for(int i=1;i<matrix.size()+1;i++){
+            for(int j=1;j<=matrix[0].size();j++){
+                pre[i][j] = pre[i-1][j]+pre[i][j-1]+matrix[i-1][j-1]-pre[i-1][j-1];
             }
         }
-
-        vector<vector<int>> res(mat.size(),vector<int>(mat[0].size()));
-        for(int i=1;i<=mat.size();i++){
-            for(int j=1;j<=mat[0].size();j++){
-                int x1 = i-k>1?i-k:1;
-                int x2 = i+k>mat.size()?mat.size():i+k;
-                int y1 = j-k>1?j-k:1;
-                int y2 = j+k>mat[0].size()?mat[0].size():j+k;
-                res[i-1][j-1] = pre[x2][y2] + pre[x1-1][y1-1] - pre[x1-1][y2] - pre[x2][y1-1];
-            }
-        }
-        return res;
-
     }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return pre[row2+1][col2+1] - pre[row1][col2+1]-pre[row2+1][col1]+pre[row1][col1];
+    }
+};
+
