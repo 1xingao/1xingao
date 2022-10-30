153a684ae0261a6041ac2d6b91ac9b8ba7bb310d
十七 <80404188+1xingao@users.noreply.github.com>
2022-10-30 15:38:49 +0800

    新增背包笔记

--- python/script/envi.py
+++ python/script/envi.py
@@ -1,5 +1,16 @@
-import shutil
+'''
+Author: xinao_seven_
+Date: 2022-08-31 22:38:00
+LastEditTime: 2022-10-29 19:22:15
+LastEditors: xinao_seven_
+Description: 
+Encoding: utf8
+FilePath: \\1xingao\\python\\script\\envi.py
 
+'''
+import shutil
+import os
+os.system("@echo off")
 path = "C:\\Users\\藏起来的十七\\.idl"
 
 try:
