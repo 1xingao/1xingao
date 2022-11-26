f3cce4aa55150aa987669b3d234809aca58aea54
十七 <80404188+1xingao@users.noreply.github.com>
2022-11-26 22:00:45 +0800

    change network

--- python/script/gui.py
+++ python/script/gui.py
@@ -1,7 +1,7 @@
 '''
 Author: xinao_seven_
 Date: 2022-11-09 09:51:53
-LastEditTime: 2022-11-09 15:36:14
+LastEditTime: 2022-11-26 19:34:40
 LastEditors: xinao_seven_
 Description: 
 Encoding: utf8
@@ -19,13 +19,19 @@ import time
 path = "D:\\abcin\\code\\1xingao\\python\\script\\Genshin_impact.py"
 
 def main():
-    time.sleep(3)
-    with open(path,"r",encoding='utf-8') as f:
-        for i in f.readlines():
-            pyautogui.typewrite(i,interval=0.1)
+    # time.sleep(3)
+    # with open(path,"r",encoding='utf-8') as f:
+    #     for i in f.readlines():
+    #         pyautogui.typewrite(i,interval=0.1)
             
-            time.sleep(1)
-
+    #         time.sleep(1)
+    mtime = 10
+    while mtime > 0:
+        pyautogui.typewrite("hhhhh")
+        time.sleep(2)
+        sizex,sizey = pyautogui.size()# get sereen size
+        pyautogui.moveTo(sizex/2,sizey/2)
+        mtime -= 1
 
 if __name__ == "__main__":
     main()
