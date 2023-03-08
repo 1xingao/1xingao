82afa2c5d26230d570ab6523c13369b920def074
十七 <80404188+1xingao@users.noreply.github.com>
2023-03-08 15:19:16 +0800

    sync to linux

--- cpp/week322.cpp
+++ cpp/week322.cpp
@@ -1,3 +1,13 @@
+/*
+ * @Author: xinao_seven_
+ * @Date: 2023-02-12 10:11:16
+ * @LastEditTime: 2023-02-24 18:15:14
+ * @LastEditors: xinao_seven_
+ * @Description: 
+ * @Encoding: utf8
+ * @FilePath: \1xingao\cpp\week322.cpp
+ * 
+ */
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -22,7 +32,7 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-class Solution {
+class Solution2 {
 public:
     vector<vector<int>> substringXorQueries(string s, vector<vector<int>>& queries) {
         vector<string> res(queries.size());
@@ -52,11 +62,79 @@ public:
         return ans;
     }
 };
-
+class Solution {
+public:
+    int minimumOperations(vector<int>& nums) {
+        priority_queue<int,vector<int>,greater<int>>q;
+        for(int n:nums){
+            if(n!=0){
+                q.push(n);
+            }
+        }
+        int res = 0;
+        while(!q.empty()){
+            int temp = q.top();
+            q.pop();
+            int n = q.size();
+            for(int i=0;i<n;i++){
+                int t = q.top();
+                q.pop();
+                t -= temp;
+                if(t>0){q.push(t);}
+            }
+            res++;
+        }
+        return res;
+    }
+};
+class Solution3 {
+public:
+    int del(string s,char c){
+        int res = 0;
+        for(char t:s){
+            if(t==c){res++;}
+        }
+        return res;
+    }
+    int minimumDeletions(string s) {
+        // int b = 0;
+        // int dp = 0;
+        // for(char c:s){
+        //     if(c=='a'){
+        //         dp = min(dp+1,b);
+        //     }else{
+        //         b++;
+        //     }
+        // }
+        // return dp;
+        int n = s.size();
+        int l =0,r = 0;
+        for(char c:s){
+            if(c =='a'){l++;}
+            else{r++;}
+        }
+        if(l==n||r==n){return 0;}
+        int res = INT_MAX;
+        int ren = 0;
+        int a = 0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='b'){
+                res = min(res,l+ren);
+                ren++;
+            }else{
+                l--;
+                res = min(res,l+ren);
+            }
+        }
+        return res;
+        
+    }
+};
 int main()
 {
-    Solution test;
-    vector<vector<int>> t{{{0,5},{1,2}}};
-    test.substringXorQueries("101101",t);
+    Solution3 test;
+    
+    string s=  "aababbab";
+    test.minimumDeletions(s);
     return 0;
 }
