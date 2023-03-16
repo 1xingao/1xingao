a909d50d487abf2043f8398d134556a9727a9971
十七 <80404188+1xingao@users.noreply.github.com>
2023-03-16 10:12:05 +0800

    解决cscode c代码报错问题

--- c/leetcode.c
+++ c/leetcode.c
@@ -1,4 +1,15 @@
+/*
+ * @Author: xinao_seven_
+ * @Date: 2023-03-10 11:03:58
+ * @LastEditTime: 2023-03-16 09:59:07
+ * @LastEditors: xinao_seven_
+ * @Description:
+ * @Encoding: utf8
+ * @FilePath: \1xingao\c\leetcode.c
+ *
+ */
 #include <stdio.h>
+#include<stdbool.h>
 bool isPalindrome(int x)
 {
     if (x < 0 || x % 10 == 0 && x != 0)
--- c/new.c
+++ c/new.c
@@ -1,6 +1,16 @@
+/*
+ * @Author: xinao_seven_
+ * @Date: 2022-10-27 21:01:16
+ * @LastEditTime: 2023-03-16 10:06:49
+ * @LastEditors: xinao_seven_
+ * @Description: 
+ * @Encoding: utf8
+ * @FilePath: \1xingao\c\new.c
+ * 
+ */
 #include<stdio.h>
 int main()
 {
-	printf("hello word\n")
+	printf("hello word\n");
 	return 0;
 }
